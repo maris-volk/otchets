5d4022131c5667e691690de84a0f5011da0da719
Maris <123395748+maris-volk@users.noreply.github.com>
2023-11-18 21:04:25 +0300

    Add files via upload
    вторая лабораторная

---
+++ dz_1_2PROG.pas
@@ -0,0 +1,21 @@
+﻿program lab_2;
+
+var
+  s: string;
+  i, half_string: byte; flag: boolean;
+begin
+  write('String: ');
+  readln(s);
+  flag := true;
+  half_string := length(s) div 2;
+  for i:=1 to half_string do
+    if Upcase(s[i]) <> Upcase(s[length(s)-i+1]) then 
+      begin
+        writeln('no palindrome');
+        flag:= false;
+        break;
+      end;
+  if flag then 
+    write('palindrome');
+  readln
+end.
