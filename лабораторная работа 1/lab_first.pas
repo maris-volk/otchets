6ee227ec3b274e2516d527ad764d342a398072d1
Maris <123395748+maris-volk@users.noreply.github.com>
2024-09-04 16:39:29 +0300

    Add files via upload

---
+++ лабораторная работа 1/lab_first.pas
@@ -0,0 +1,28 @@
+﻿program Volk;
+
+var
+  x, y: real;
+
+function first(x: real): real;
+begin
+  if x < -8 then
+  y := -x - 6
+  else if (x >= -8) and (x < -4) then
+    y := 21
+  else if (x >= -4) and (x <= 3) then
+    y := (ln(x) / ln(10)) * 12   
+  else if x > 3 then
+    y := x * Power(x, 0.1 * x) + 47 / 36;  
+
+  Result := y;
+end;
+
+begin
+  x := -10;
+  while x <= 5 do
+  begin
+    y := first(x);
+    writeln('x = ', x:0:1, ', y = ', y:0:2);
+    x := x + 0.1;
+  end;
+end.
